85c88d8db2c2ecff71f4bb997d73f31b3e651c9a
MIDOZZ <73133130+midozz@users.noreply.github.com>
2021-04-27 12:34:36 +0200

    v0.1 Beta

---
+++ ratlang/ratlang.py
@@ -0,0 +1,9 @@
+Public Class tools
+    Public Shared Sub message(text As Object)
+        MsgBox(text)
+    End Sub
+    Public Shared Sub consolelog(log As Object)
+        Console.Write(log)
+    End Sub
+
+End Class
